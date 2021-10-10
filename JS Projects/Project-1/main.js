f40dbb1286d0ac254b4fdaf4f49e74266ca191d8
Dariusz Reczek <dariusz.reczek@gmail.com>
2021-10-10 19:07:22 +0200

    add logic

--- JS Projects/Project-1/main.js
+++ JS Projects/Project-1/main.js
@@ -7,24 +7,60 @@ let computerChoice = "";
 rockDiv.addEventListener("click", () => {
   userChoice = getUserChoiceItem("rock");
   computerChoice = getComputerChoice();
+  console.log("user", userChoice);
+  console.log("comp", computerChoice);
+  compare();
 });
 
 paperDiv.addEventListener("click", () => {
   userChoice = getUserChoiceItem("paper");
   computerChoice = getComputerChoice();
+  console.log("user", userChoice);
+  console.log("comp", computerChoice);
+  compare();
 });
 scissorsDiv.addEventListener("click", () => {
   userChoice = getUserChoiceItem("scissors");
   computerChoice = getComputerChoice();
+  console.log("user", userChoice);
+  console.log("comp", computerChoice);
+  compare();
 });
 
 function getUserChoiceItem(myChoice) {
-  console.log("user", myChoice);
+  // console.log("user", myChoice);
   return myChoice;
 }
-
 function compare() {
-
+  if (userChoice === "rock" && computerChoice === "paper") {
+    console.log("user", userChoice);
+    console.log("comp", computerChoice);
+    console.log("wygrana");
+  }
+  if (userChoice === "rock" && computerChoice === "scissors") {
+    console.log("wygrana");
+  }
+  if (userChoice === "rock" && computerChoice === "rock") {
+    console.log("remis");
+  }
+  if (userChoice === "paper" && computerChoice === "rock") {
+    console.log("wygrana");
+  }
+  if (userChoice === "paper" && computerChoice === "scissors") {
+    console.log("wygrana");
+  }
+  if (userChoice === "paper" && computerChoice === "paper") {
+    console.log("remis");
+  }
+  if (userChoice === "scissors" && computerChoice === "paper") {
+    console.log("wygrana");
+  }
+  if (userChoice === "scissors" && computerChoice === "rock") {
+    console.log("przegrana");
+  }
+  if (userChoice === "scissors" && computerChoice === "scissors") {
+    console.log("remis");
+  }
 }
 
 // function computerChoiceItem(compChoice) {
@@ -46,4 +82,15 @@ function getComputerChoice() {
   }
 }
 
+// computerChoice();
 
+// function compare(comaprison) {
+//   console.log("uruchomiona");
+//   if (userChoice() === "rock" && computerChoice() === "rock") {
+//     console.log("remis");
+//   } else {
+//     console.log("blad");
+//   }
+// }
+
+// compare();
