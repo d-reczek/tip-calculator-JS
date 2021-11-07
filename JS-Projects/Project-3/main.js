744e2447f2add60bff709e5debbed8420418aa61
Dariusz Reczek <dariusz.reczek@gmail.com>
2021-11-07 22:01:01 +0100

    create board create pacman move pacman

--- JS-Projects/Project-3/main.js
+++ JS-Projects/Project-3/main.js
@@ -1,5 +1,5 @@
 function init() {
-  console.log("dziaała");
+  width = 28;
   const grid = document.querySelector(".grid");
   const layout = [
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -40,18 +40,77 @@ function init() {
   // 2 - ghost-lair
   // 3 - power-pellet
   // 4 - empty
-  let squares = [];
+  const squares = [];
 
+  // create your board
   function createBoard() {
     for (let i = 0; i < layout.length; i++) {
-      let square = document.createElement("div");
+      const square = document.createElement("div");
       grid.appendChild(square);
       squares.push(square);
+
+      //add layout to the board
       if (layout[i] === 0) {
-        squares[i].classList.add("pac-dot");
+        squares[i].classList.add("pac-dot", i);
+      } else if (layout[i] === 1) {
+        squares[i].classList.add("wall", i);
+      } else if (layout[i] === 2) {
+        squares[i].classList.add("ghost-lair", i);
+      } else if (layout[i] === 3) {
+        squares[i].classList.add("power-pellet", i);
+      } else if (layout[i] === 4) {
+        squares[i].classList.add(i);
       }
     }
   }
   createBoard();
+
+  let pacManPosition = 490;
+  function createPacMan() {
+    squares[pacManPosition].classList.add("pac-man");
+  }
+  createPacMan();
+
+  document.addEventListener("keyup", (event) => {
+    squares[pacManPosition].classList.remove("pac-man");
+
+    if (event.keyCode === 37) {
+      squares[(pacManPosition -= 1)].classList.add("pac-man");
+    }
+    if (event.keyCode === 39) {
+      squares[(pacManPosition += 1)].classList.add("pac-man");
+    }
+    if (event.keyCode === 38) {
+      squares[(pacManPosition -= width)].classList.add("pac-man");
+    }
+    if (event.keyCode === 40) {
+      squares[(pacManPosition += width)].classList.add("pac-man");
+    }
+  });
+  // let squares = [];
+  // layout.forEach((element) => {
+  //   let num = "";
+  //   for (i = 0; i > layout.length; i++) {
+  //     num = i;
+  //   }
+
+  //   let square = document.createElement("div");
+  //   square.classList.add(element +1)
+  //   grid.appendChild(square);
+  //   squares.push(square);
+  //   if (element === 0) {
+  //     square.classList.add("pac-dot");
+  //   }
+  //   if (element === 1) {
+  //     square.classList.add("wall");
+  //   }
+  //   if (element === 2) {
+  //     square.classList.add("ghost-lair");
+  //   }
+  //   if (element === 3) {
+  //     square.classList.add("power-pellet");
+  //   }
+  // });
+  // console.log(squares);
 }
 window.onload = init;
