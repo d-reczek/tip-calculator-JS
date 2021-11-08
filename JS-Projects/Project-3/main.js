fa79dfdbe7641e508a981863ec71ab3498eb4138
Dariusz Reczek <dariusz.reczek@gmail.com>
2021-11-08 21:42:50 +0100

    add pac man eat dot and score

--- JS-Projects/Project-3/main.js
+++ JS-Projects/Project-3/main.js
@@ -1,4 +1,5 @@
 function init() {
+  const scoreSpan = document.querySelector(".score");
   width = 28;
   const grid = document.querySelector(".grid");
   const layout = [
@@ -73,44 +74,46 @@ function init() {
 
   document.addEventListener("keyup", (event) => {
     squares[pacManPosition].classList.remove("pac-man");
-
-    if (event.keyCode === 37) {
+    if (
+      event.keyCode === 37 &&
+      !squares[pacManPosition - 1].classList.contains("wall")
+    ) {
       squares[(pacManPosition -= 1)].classList.add("pac-man");
     }
-    if (event.keyCode === 39) {
+    if (
+      (event.keyCode === 39) &
+      !squares[pacManPosition + 1].classList.contains("wall")
+    ) {
       squares[(pacManPosition += 1)].classList.add("pac-man");
     }
-    if (event.keyCode === 38) {
+    if (
+      event.keyCode === 38 &&
+      !squares[pacManPosition - width].classList.contains("wall")
+    ) {
       squares[(pacManPosition -= width)].classList.add("pac-man");
     }
-    if (event.keyCode === 40) {
+    if (
+      event.keyCode === 40 &&
+      !squares[pacManPosition + width].classList.contains("wall")
+    ) {
       squares[(pacManPosition += width)].classList.add("pac-man");
     }
+    squares[pacManPosition].classList.add("pac-man");
+    console.log(pacManPosition);
+    console.log("score", score);
+    pacManEatDot();
   });
-  // let squares = [];
-  // layout.forEach((element) => {
-  //   let num = "";
-  //   for (i = 0; i > layout.length; i++) {
-  //     num = i;
-  //   }
-
-  //   let square = document.createElement("div");
-  //   square.classList.add(element +1)
-  //   grid.appendChild(square);
-  //   squares.push(square);
-  //   if (element === 0) {
-  //     square.classList.add("pac-dot");
-  //   }
-  //   if (element === 1) {
-  //     square.classList.add("wall");
-  //   }
-  //   if (element === 2) {
-  //     square.classList.add("ghost-lair");
-  //   }
-  //   if (element === 3) {
-  //     square.classList.add("power-pellet");
-  //   }
-  // });
+  let score = 0;
+  function pacManEatDot() {
+    if (squares[pacManPosition].classList.contains("pac-dot")) {
+      console.log("jest kropka");
+      squares[pacManPosition].classList.remove("pac-dot");
+      score += 1;
+      scoreSpan.textContent = score;
+    }
+  }
+  console.log(score);
   // console.log(squares);
+  // console.log(layout);
 }
 window.onload = init;
