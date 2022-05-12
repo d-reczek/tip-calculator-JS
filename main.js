e1fac7637381c02704a80131d001e0cc4a11e749
Dariusz Reczek <dariusz.reczek@gmail.com>
2022-05-12 20:31:26 +0200

    delete console log

--- main.js
+++ main.js
@@ -24,45 +24,23 @@ function init() {
 
   rangeInput.addEventListener("change", function () {
     tipCalculate();
-    console.log(billInputValue);
   });
 
   billInput.addEventListener("change", () => {
     tipCalculate();
   });
 
-  // billInputValue.addEventListener("change", function () {
-  //   tipCalculate();
-  // });
-
-  // let buttonPerecent10Value = "";
   button10Percent.addEventListener("click", function (event) {
     event.preventDefault();
     let buttonPerecent10Value = (rangeInput.value = 10);
 
     tipCalculate();
-    // let rangeInputValue = buttonPerecent10Value / 100;
-    // let billInputValue = parseFloat(billInput.value);
-    // percent.innerText = rangeInput.value + "%";
-    // let result = billInputValue * rangeInputValue;
-    // let billPlusTip = result + billInputValue;
-    // totalAmount.innerText = billPlusTip.toFixed(2);
-    // tipValue.innerText = result.toFixed(2) + "zł";
-    // console.log(buttonPerecent10Value);
   });
 
-  // let buttonPerecent20Value = "";
   button20Percent.addEventListener("click", function (event) {
     event.preventDefault();
     let buttonPerecent20Value = (rangeInput.value = 20);
     tipCalculate();
-    // let rangeInputValue = buttonPerecent20Value / 100;
-    // let billInputValue = parseFloat(billInput.value);
-    // percent.innerText = rangeInput.value + "%";
-    // let result = billInputValue * rangeInputValue;
-    // let billPlusTip = result + billInputValue;
-    // totalAmount.innerText = billPlusTip.toFixed(2);
-    // tipValue.innerText = result.toFixed(2) + "zł";
   });
 }
 
